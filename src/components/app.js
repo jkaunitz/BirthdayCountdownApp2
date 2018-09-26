7fdf2ee1af58b31640060eddbbc0a08a4105a242
Justin Kaunitz <justin.kaunitz@gmail.com>
2018-09-26 00:01:10 -0700

    initial project

---
+++ src/components/app.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+
+import Picker from './picker';
+import Button from './button';
+import Clock from './clock';
+import ChangeDate from './changeDate';
+import LargeText from './largeText';
+import { isMoment } from 'moment';
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.timer = 0;
+
+    this.state = {
+      active: false,
+      startDate: isMoment(),
+      timeRemaining: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      },
+      age: 0
+    };
+
+    this.handleGenerate = this.handleGenerate.bind(this);
+  }
+
+  handleChange = function(date) {
+    console.log("APP JS HANDLE CHANGE", date._d);
+
+    this.setState({
+      startDate: date
+    });
+  }.bind(this);
+
+  handleGenerate = function() {
+    this.setState({ active: true });
+
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
+
+    if(birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDay();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear()) 
+      } 
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      } 
+    }
+
+    var countDownDate = this.state.startDate.toDate().getTime();
+
+    this.timer = setInterval(function() {
+
+      var now = today.getTime();
+      var distance = countDownDate - now;
+      
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      const timeRemaining = {
+        days,
+        hours,
+        minutes,
+        seconds
+      };
+
+      this.setState({ timeRemaining });
+      
+      if (distance < 0) {
+          clearInterval(x);
+          // document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    }.bind(this)
+
+
+  renderItems = function() {
+    if(this.state.active) {
+      return [
+        <Clock/>, 
+        ChangeDate('Change Date', () => this.setState({ active: false})),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your 21st birthday</label>
+      ]
+    } else {
+      return [
+       <Picker/> ,
+       Button('Generate Countdown', () => this.handleGenerate())
+      ]
+    }
+  }.bind(this)
+
+  render() {
+    return (
+      <div className="grid">
+        <h1 className="grid__title">Birthday Countdown</h1>
+
+        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-three"></div>
+
+        <div className="grid__skew-light-one"></div>
+        <div className="grid__skew-light-two"></div>
+        <div className="grid__skew-light-three-box"></div>
+
+        { this.renderItems() }
+        
+      </div>
+    );
+  }
+}
